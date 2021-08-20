91c2fc28f7a8ed556a20a56266636c2efe082b7c
Martcant2021 <martincantero31@gmail.com>
2021-08-20 14:21:57 -0300

    Formulario de login

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -14,24 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+#from django.conf.urls import url
 from django.urls import path
 from preguntas import views
 
 
 
 urlpatterns = [
+    path('', views.inicio, name='inicio' ),
+    #url(r'^$', views.inicio, name='inicio'),
     path('admin/', admin.site.urls),
     path('jugar/', views.jugar),
-    path('inicio/', views.inicio ),
+    path('login/', views.vista_login ),
     path('registro/', views.registro ),
-    path('login/', views.login ),
-
-    #path('login/', views ),#Iria una view de login
-
-   
-    # path('login/', views ),#Iria una view de login
-
-    #path('jugar/', views ),#Iria una view para jugar
-    #'''Creo que tambien una view para rankings, estadisticas, compartir
-    #(podria ir todo junto en una app)'''
+#'''Creo que tambien una view para rankings, estadisticas, compartir
+#(podria ir todo junto en una app)'''
 ]
--- preguntas/views.py
+++ preguntas/views.py
@@ -1,13 +1,15 @@
 from django import forms
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-from .forms import RegistroFormulario
+from .forms import RegistroFormulario, IniciarSesionForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def inicio(request):
     return render(request,'quiz/inicio.html', context=None)
 
-    
+
 
 def registro(request):
     titulo = 'Crear Usuario'
@@ -22,7 +24,7 @@ def registro(request):
         'form' : form,
         'titulo': titulo,
     }
-    
+
     return render(request, 'Usuario/registro.html', context)
 
 
@@ -30,7 +32,18 @@ def jugar(request):
 
     return render(request, 'quiz/jugar.html', context=None)
 
-def login(request):
-    return render(request, 'Usuario/login.html', context=None)
+# Intento de inicio de sesion
+
+def vista_login(request):
+    title = "Iniciar Sesion"
+    form = IniciarSesionForm(request.POST or None)
+    if form.is_valid():
+        usuario = form.cleaned_data.get("Nombre de usuario")
+        contraseña = form.cleaned_data.get("contraseña")
+        user = authenticate(username=usuario, password=contraseña)
+        login(request,user)
+        return redirect('')
+
+    return render(request, 'Usuario/login.html',{"form":form, "titulo":title})
 
 
